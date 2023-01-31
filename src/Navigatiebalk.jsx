139a0ad84f41658248e598f9313960f99f07fde5
rosanvdlinden <>
2023-01-31 16:15:52 +0000

    Initial commit

---
+++ src/Navigatiebalk.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+class App extends React.Component {
+  render() {
+    return (
+      <main>
+        <div class="navbar">
+          <a class="active" href="index.html">Home</a>
+          <div class="navbar-right">
+            <a href="index.html">Uitloggen</a>
+          </div>
+        </div> 
+      </main>
+    )
+  }
+}
