c0543919ea825ff39d1899fb1c391de831287c06
RosanvanderLinden <rosan@vander-linden.nl>
2023-02-03 22:51:17 +0100

    elementen.css gemaakt en gelinkt

--- src/Elementen.jsx
+++ src/Elementen.jsx
@@ -1,18 +1,21 @@
 import React from 'react';
+import 'Elementen.css';
 class Elementen extends React.Component {
 
     render() {
         const {titel, beschrijving, image, link} = this.props.element;
 
-        return <div className="grid-item">
-            <div className="card">
-                <div className="card-body">
-                    <img className="card-image" src={image} alt={"test"}></img>
-                    <h3 className="card-title">{titel}</h3>
-                    <p className="card-text">{beschrijving}</p>
-                    <button className="card-button-link">Naar uw {link}→</button>
+        return <div className={"canvasElement " + id}>
+                <div className="grid-item">
+                    <div className="card">
+                        <div className="card-body">
+                            <img className="card-image" src={image} alt={"test"}></img>
+                            <h3 className="card-title">{titel}</h3>
+                            <p className="card-text">{beschrijving}</p>
+                            <button className="card-button-link">Naar uw {link}→</button>
+                        </div>
+                    </div>
                 </div>
-            </div>
         </div>
     }
 }
